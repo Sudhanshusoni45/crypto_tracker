36bd9979f897ae39c6258f930f1c41fbe196e026
Sudhanshusoni45 <sudhanshusoni45@gmail.com>
2022-07-01 21:57:35 +0530

    feat:wired mobile popup

--- src/pages/homepage/homepage.jsx
+++ src/pages/homepage/homepage.jsx
@@ -1,17 +1,36 @@
 import { useEffect, useState } from "react";
 import { down_arrow } from "../../assests";
 import { Card } from "../../components/card/Card";
+import { MobilePopup } from "../../components/mobile_popup/MobilePopup";
 import { Navbar } from "../../components/navbar/Navbar";
 import { getCoinsDataHandler } from "../../util/getCoinsDataHandler";
 import "./homepage.css";
 
 const Homepage = () => {
   const [coinData, setCoinData] = useState("");
+  const [singleCoinData, setSingleCoinData] = useState("");
+  const [showMobilePopup, setShowMobilePopup] = useState(false);
+
+  const getSingleCoinData = (id) => coinData.find((coin) => coin.id === id);
+
+  const mobilePopupHandler = (id) => {
+    setShowMobilePopup((prevState) => true);
+    const foundCoinData = getSingleCoinData(id);
+    setSingleCoinData((prevState) => foundCoinData);
+  };
+
   useEffect(() => {
     getCoinsDataHandler(setCoinData);
   }, []);
+
   return (
     <>
+      {showMobilePopup ? (
+        <MobilePopup
+          setShowMobilePopup={setShowMobilePopup}
+          singleCoinData={singleCoinData}
+        />
+      ) : null}
       <Navbar />
       <div className="homepage_slider">
         <Card />
@@ -25,58 +44,64 @@ const Homepage = () => {
       <div className="homepage_heading">
         <h3>Top 100 Cryptocurrencies by Market Cap</h3>
       </div>
-      <table className="coinsTable">
-        <thead>
-          <tr>
-            <th></th>
-            <th colSpan={2} className="text_align_left">
-              NAME
-            </th>
-            <th colSpan={1} className="text_align_left">
-              PRICE
-            </th>
-            <th className="">
-              24H
-              <img src={down_arrow} alt="" className="display_inline" />
-            </th>
-          </tr>
-        </thead>
-        {coinData ? (
-          coinData.map(
-            ({
-              id,
-              name,
-              image: imageUrl,
-              symbol,
-              current_price,
-              price_change_percentage_24h,
-            }) => (
-              <tr key={id}>
-                <td>
-                  <button className="transparent_btn">
-                    <i className="far fa-star"></i>
-                  </button>
-                </td>
-                <td>
-                  <img src={imageUrl} alt={name} className="coinImage" />
-                </td>
-                <td>
-                  {name} <span className="grey_text">{symbol}</span>
-                </td>
+      <div className="coinsTable_container">
+        <table className="coinsTable">
+          <thead>
+            <tr>
+              <th></th>
+              <th colSpan={2} className="text_align_left">
+                NAME
+              </th>
+              <th colSpan={1} className="text_align_left">
+                PRICE
+              </th>
+              <th className="text_align_left">
+                24H
+                <img src={down_arrow} alt="" className="display_inline" />
+              </th>
+            </tr>
+          </thead>
+          {coinData ? (
+            coinData.map(
+              ({
+                id,
+                name,
+                image: imageUrl,
+                symbol,
+                current_price,
+                price_change_percentage_24h,
+              }) => (
+                <tr
+                  key={id}
+                  onClick={() => mobilePopupHandler(id)}
+                  className="cursor_pointer"
+                >
+                  <td>
+                    <button className="transparent_btn">
+                      <i className="far fa-star"></i>
+                    </button>
+                  </td>
+                  <td>
+                    <img src={imageUrl} alt={name} className="coinImage" />
+                  </td>
+                  <td>
+                    {name} <span className="grey_text">{symbol}</span>
+                  </td>
 
-                <td>{current_price}</td>
-                <td className="red_text coin_24h_change">
-                  {parseInt(price_change_percentage_24h)}%
-                </td>
-              </tr>
+                  <td>${current_price}</td>
+                  <td className="red_text coin_24h_change">
+                    {parseInt(price_change_percentage_24h)}%
+                  </td>
+                </tr>
+              )
             )
-          )
-        ) : (
-          <tr>
-            <td>Loading...</td>
-          </tr>
-        )}
-      </table>
+          ) : (
+            <tr>
+              <td>Loading...</td>
+            </tr>
+          )}
+        </table>
+      </div>
     </>
   );
 };
