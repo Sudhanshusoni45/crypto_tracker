6ee0f27f72b0b5a549475b5757749d2b85a67cfa
Sudhanshusoni45 <sudhanshusoni45@gmail.com>
2022-07-02 20:04:55 +0530

    style:add color to cryptoCurrency

--- src/components/category_chip/CategoryChip.jsx
+++ src/components/category_chip/CategoryChip.jsx
@@ -4,7 +4,15 @@ const CategoryChip = ({ category_name, category_icon }) => {
   return (
     <div className="category_chip_container">
       {category_icon ? <img src={category_icon} alt={category_name} /> : null}
-      <small>{category_name}</small>
+      <small
+        className={
+          category_name === "CryptoCurrencies"
+            ? "active_link_color xs_margin_left"
+            : "xs_margin_left"
+        }
+      >
+        {category_name}
+      </small>
     </div>
   );
 };
